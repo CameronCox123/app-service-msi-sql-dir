3526e75563694dffe42ea9f2e0daa1f825095176
Cameron Cox <82111924+CameronCox123@users.noreply.github.com>
2024-06-14 14:50:22 -0500

    Update just-app-service.tf

--- just-app-service.tf
+++ just-app-service.tf
@@ -18,17 +18,11 @@ resource "random_integer" "ri" {
   max = 99999
 }
 
-# Create the resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
-  location = "eastus"
-}
-
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "eastus"
+  resource_group_name = "myResourceGroup-15330"
   os_type             = "Linux"
   sku_name            = "B1"
 }
@@ -36,20 +30,11 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
   name                  = "webapp-${random_integer.ri.result}"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  location              = "eastus"
+  resource_group_name   = "myResourceGroup-15330"
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   https_only            = true
   site_config { 
     minimum_tls_version = "1.2"
   }
-}
-
-#  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch             = "master"
-  use_manual_integration = true
-  use_mercurial      = false
 }
