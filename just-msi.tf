198e84b427e560b2d74857be90b2957bf4995df7
Cameron Cox <82111924+CameronCox123@users.noreply.github.com>
2024-06-14 15:40:22 -0500

    Update just-msi.tf

--- just-msi.tf
+++ just-msi.tf
@@ -1,3 +1,18 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_user_assigned_identity" "example" {
   location            = "eastus"
   name                = "example"
