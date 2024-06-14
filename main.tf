5b4971db5173b0a0963e46a4955a9d559926e1fa
Cameron Cox <82111924+CameronCox123@users.noreply.github.com>
2024-06-14 12:57:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,15 +51,6 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 }
 
-# Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch                 = "master"
-  use_manual_integration = true
-  use_mercurial          = false
-}
-
 # Output the Principal ID of the MSI
 output "msi_principal_id" {
   value = azurerm_linux_web_app.webapp.identity[0].principal_id
