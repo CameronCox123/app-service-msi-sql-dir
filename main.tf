6b28d5b8390c13784ae885c211e0a876b0145a33
Cameron Cox <82111924+CameronCox123@users.noreply.github.com>
2024-06-14 12:40:25 -0500

    Update and rename create-app-service.tf to main.tf

--- main.tf
+++ main.tf
@@ -64,3 +64,31 @@ resource "azurerm_app_service_source_control" "sourcecontrol" {
 output "msi_principal_id" {
   value = azurerm_linux_web_app.webapp.identity[0].principal_id
 }
+
+resource "azurerm_mssql_server" "server" {
+  name                         = "my-sql-server"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  administrator_login          = "campatcox@gmail.com"
+  administrator_login_password = "4PangoLinMM$"
+  version                      = "12.0"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name                = "my-sql-db"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_id           = azurerm_mssql_server.server.id
+}
+
+# Assign MSI of App Service as Azure AD Admin on SQL Server
+resource "azurerm_sql_active_directory_administrator" "example" {
+  server_name         = "my-sql-server"
+  resource_group_name = azurerm_resource_group.rg.name
+  login               = azurerm_linux_web_app.webapp.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  object_id           = azurerm_linux_web_app.webapp.identity[0].principal_id
+}
+
+# Data source to get the current tenant ID
+data "azurerm_client_config" "current" {}
